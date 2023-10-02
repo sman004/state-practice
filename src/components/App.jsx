eae36461bd051e1266e21cd772f89010a9f70886
Suleiman Ibrahim <36508732+sman004@users.noreply.github.com>
2023-10-02 16:55:41 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(1);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div>
+      <h1> {count}</h1>
+      <button onClick={increaseCount}>+</button>
+      <button onClick={decreaseCount}>-</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <div className="container">
+    <div>
+      <App />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
